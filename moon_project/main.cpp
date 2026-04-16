bacf0312d5b001f647565079e98854ed36e16dc6
nochulu <oborovairina81@gmail.com>
2026-04-16 20:53:17 +0800

    Add DateTime project and ignore large moon data

---
+++ moon_project/main.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+bool isValid(int y, int m, int d) {
+    if (y < 1900 || y > 2100 || m < 1 || m > 12 || d < 1) return false;
+    int days[13] = { 0,31,28,31,30,31,30,31,31,30,31,30,31 };
+    if (m == 2 && ((y % 4 == 0 && y % 100 != 0) || y % 400 == 0)) days[2] = 29;
+    return d <= days[m];
+}
+
+string formatTimeStr(int hms) {
+    int hh = hms / 10000;
+    int mm = (hms % 10000) / 100;
+    int ss = hms % 100;
+    string res = "";
+    if (hh < 10) res += "0"; res += to_string(hh) + ":";
+    if (mm < 10) res += "0"; res += to_string(mm) + ":";
+    if (ss < 10) res += "0"; res += to_string(ss);
+    return res;
+}
+
+int main() {
+    int day, month, year;
+    char dot1, dot2;
+
+    cout << "Enter date (dd.mm.yyyy): ";
+    if (!(cin >> day >> dot1 >> month >> dot2 >> year)) return 1;
+
+    if (dot1 != '.' || dot2 != '.' || !isValid(year, month, day)) {
+        cout << "Error: Invalid date!" << endl;
+        return 1;
+    }
+
+    string filename = "Moon/Moon/moon" + to_string(year) + ".dat";
+    ifstream file(filename);
+
+    if (!file.is_open()) {
+        cout << "Error: File " << filename << " not found!" << endl;
+        return 1;
+    }
+
+    string header;
+    getline(file, header); 
+
+    string rise = "not found";
+    string culmin = "not found";
+    string set_time = "not found";
+
+    double maxEl = -1000.0;
+    double prevEl = -1000.0;
+    bool dateFound = false;
+    bool firstPointOfDate = true;
+
+    long long targetYMD = (long long)year * 10000 + month * 100 + day;
+
+    long long ymd;
+    int hms;
+    double t, r, el, az, fi, lg;
+
+    while (file >> ymd >> hms >> t >> r >> el >> az >> fi >> lg) {
+        if (ymd == targetYMD) {
+            dateFound = true;
+
+            if (el > maxEl) {
+                maxEl = el;
+                culmin = formatTimeStr(hms);
+            }
+
+            if (!firstPointOfDate) {
+                if (prevEl < 0 && el >= 0 && rise == "not found") {
+                    rise = formatTimeStr(hms);
+                }
+                if (prevEl > 0 && el <= 0 && set_time == "not found") {
+                    set_time = formatTimeStr(hms);
+                }
+            }
+
+            prevEl = el;
+            firstPointOfDate = false;
+        } 
+        else if (dateFound) {
+            break;
+        }
+    }
+
+    file.close();
+
+    if (!dateFound) {
+        cout << "No data for this date." << endl;
+    } else {
+        cout << "\nDate: " << day << "." << month << "." << year << endl;
+        cout << "Moonrise:    " << rise << endl;
+        cout << "Culmination: " << culmin << endl;
+        cout << "Moonset:     " << set_time << endl;
+    }
+
+    cout << endl;
+    system("pause");
+
+    return 0;
+}
